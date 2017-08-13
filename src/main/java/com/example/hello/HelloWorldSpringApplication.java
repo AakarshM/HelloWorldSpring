b3b5ec7fa822b6bb6cb9e203a86d811f2224c610
AakarshM <a3madhav@edu.uwaterloo.ca>
2017-08-13 08:13:36 -0400

    commit

--- src/main/java/com/example/hello/HelloWorldSpringApplication.java
+++ src/main/java/com/example/hello/HelloWorldSpringApplication.java
@@ -2,11 +2,25 @@ package com.example.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class HelloWorldSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldSpringApplication.class, args);
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+		//Load spring config
+
+		Coach currentCoach = context.getBean("myCoach", Coach.class);
+
+		//Retrieve bean from spring container
+
+		currentCoach.getWorkout();
+
+		//Call methods on the bean
+		context.close();
+		
 	}
 }
